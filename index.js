536b926b8db271b722ea9ba86b6e09f82e545429
dumpy <dumpyee09@gmail.com>
2022-09-10 20:05:37 +1000

    tests

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const { log, logadv } = require('@dumpy/andylib')
-
+const al = require('@dumpy/andylib');
+const l = new al.logger();
+const moment = require('moment');
+app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', 'true');
     res.header('content-type', 'application/json');
     next();
 })
@@ -15,138 +18,65 @@ app.use((req, res, next) => {
 app.get('/v1/all/:d/:m/:y', async (req, res) => {
     console.log(req.params)
     const { d, m, y } = req.params;
-    const date = new Date(`${y}-${m}-${d}`);
-
-    const years = yearsUntil(date);
-    const months = monthsUntil(date);
-    const days = daysUntil(date);
-    const hours = hoursUntil(date);
-    const mins = minutesUntil(date);
-    const seconds = secondsUntil(date);
+    const years = yearsUntil(d, m, y);
+    const months = monthsUntil(d, m, y);
+    const days = daysUntil(d, m, y);
+    const hours = hoursUntil(d, m, y);
+    const minutes = minutesUntil(d, m, y);
+    const seconds = secondsUntil(d, m, y);
 
     return res.status(200).json({
         years: years,
         months: months,
         days: days,
         hours: hours,
-        mins: mins,
+        minutes: minutes,
         seconds: seconds
     });    
 });
 
-app.get('/v1/secondsuntil/:d/:m/:y', async (req, res) => {
-    const { d, m, y } = req.params;
-    const date = new Date(`${d}/${m}/${y}`);
-    
-    const seconds = secondsUntil(date);
-    res.status(200).json({
-        seconds: seconds
-    })
-})
-app.get('/v1/minsuntil/:d/:m/:y', async (req, res) => {
-    const { d, m, y } = req.params;
-    const date = new Date(`${d}/${m}/${y}`);
-    
-    const mins = minutesUntil(date);
-    res.status(200).json({
-        minutes: mins
-    });
-});
-app.get('/v1/monthsuntil/:d/:m/:y', async (req, res) => {
-    const { d, m, y } = req.params;
-    const date = new Date(`${d}/${m}/${y}`);
-    
-    const months = monthsUntil(date);
-    return res.status(200).json({
-        months: months
-    });
-
-});
-app.get('/v1/hoursuntil/:d/:m/:y', async (req, res) => {
-    const { d, m, y } = req.params;
-    const date = new Date(`${d}/${m}/${y}`);
-
-    const hours = hoursUntil(date);
-    return res.status(200).json({
-        hours: hours
-    });
-});
-
 app.get('/v1/yearsuntil/:d/:m/:y', async (req, res) => {
     const { d, m, y } = req.params;
-    const date = new Date(`${d}/${m}/${y}`);
-    
-    const years = yearsUntil(date);
+    const years = yearsUntil(d, m, y);
     return res.status(200).json({
         years: years
     });
-});
+})
+app.get('/v1/monthsuntil/:d/:m/:y', async (req, res) => {
+    const { d, m, y } = req.params;
+    const months = monthsUntil(d, m, y);
+    return res.status(200).json({
+        months: months
+    });
+})
 app.get('/v1/daysuntil/:d/:m/:y', async (req, res) => {
     const { d, m, y } = req.params;
-    const date = new Date(`${d}/${m}/${y}`);
-
-    const days = daysUntil(date);
+    const days = daysUntil(d, m, y);
     return res.status(200).json({
         days: days
     });
-});
-
-
-
-app.get('/v1/timeuntil/:d/:m/:y/', async (req, res) => {
-    const { d, m, y } = req.params;
-    if (!(d && m && y)) {
-        return res.status(400).json({
-            error: 'Missing parameters'
-        });
-    }
-
-    const time = new Date(y, m-1, d);
-    const now = new Date();
-
-    const diff = time.getTime() - now.getTime();
-    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-
-    // difference between the current date and the date of the event
-    const result = {
-        days,
-        hours,
-        minutes,
-        seconds
-    };
-    return res.status(200).json(result);
 })
-
-app.get('/v1/timepast/:d/:m/:y/', async (req, res) => {
-    if (!(req.params.d && req.params.m && req.params.y)) {
-        return res.status(400).json({
-            error: 'Missing parameters'
-        });
-    }
-
-    const time = new Date(req.params.y, req.params.m - 1, req.params.d);
-    const now = new Date();
-
-    const diff = now.getTime() - time.getTime();
-    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-    const years = Math.floor(days / 365);
-
-    return res.json({
-        years: years,
-        days: days,
-        hours: hours,
-        minutes: minutes,
-        seconds: seconds,
-        timestamp: time.getTime()
+app.get('/v1/hoursuntil/:d/:m/:y', async (req, res) => {
+    const { d, m, y } = req.params;
+    const hours = hoursUntil(d, m, y);
+    return res.status(200).json({
+        hours: hours
     });
 })
-
+app.get('/v1/minutesuntil/:d/:m/:y', async (req, res) => {
+    const { d, m, y } = req.params;
+    const minutes = minutesUntil(d, m, y);
+    return res.status(200).json({
+        minutes: minutes
+    });
+})
+app.get('/v1/secondsuntil/:d/:m/:y', async (req, res) => {
+    const { d, m, y } = req.params;
+    const seconds = secondsUntil(d, m, y);
+    return res.status(200).json({
+        seconds: seconds
+    });
+})
 app.use((req, res, next) => {
     res.status(404)
     // respond with json
@@ -164,50 +94,55 @@ app.use((req, res, next) => {
 
 /* Telling the server to listen on port 3000. */
 const server = app.listen(4200, () => {
-    log('listening on port 4200', 'info')
+    l.info('listening on port 4200', 'info')
 });
 
 server.on('connection', (socket) => {
-    log(`New request from ${socket.localAddress}:${socket.localPort}`, 'info')
+    l.info(`New request from ${socket.localAddress}:${socket.localPort}`, 'info')
 })
 server.on('disconnect', (socket) => {
-    log(`Disconnected from ${socket.localAddress}:${socket.localPort}`, 'info')
+    l.info(`Disconnected from ${socket.localAddress}:${socket.localPort}`, 'info')
 })
-
-function secondsUntil(date) {
-    const now = new Date();
-    const diff = date.getTime() - now.getTime();
-    return Math.floor(diff / 1000);
+function until(day, month, year) {
+    const until = moment(`${year}-${month}-${day}`, 'YYYY-MM-DD');
+    const duration = moment.duration(until.diff(this.now));
+    return {
+        years: duration.years(),
+        months: duration.months(),
+        days: duration.days(),
+        hours: duration.hours(),
+        minutes: duration.minutes(),
+        seconds: duration.seconds()
+    };
 }
-
-function monthsUntil(date) {
-    const now = new Date();
-    const diff = date.getTime() - now.getTime();
-    return Math.floor(diff / (1000 * 60 * 60 * 24 * 30));
+function yearsUntil(day, month, year) {
+    const until = moment(`${year}-${month}-${day}`, 'YYYY-MM-DD');
+    const duration = moment.duration(until.diff(this.now));
+    return duration.years();
 }
-
-function minutesUntil(date) {
-    const now = new Date();
-    const diff = date.getTime() - now.getTime();
-    return Math.floor(diff / (1000 * 60));
+function monthsUntil(day, month, year) {
+    const until = moment(`${year}-${month}-${day}`, 'YYYY-MM-DD');
+    const duration = moment.duration(until.diff(this.now));
+    return duration.months();
 }
-
-function hoursUntil(date) {
-    const now = new Date();
-    const diff = date.getTime() - now.getTime();
-    return Math.floor(diff / (1000 * 60 * 60));
+function daysUntil(day, month, year) {
+    const until = moment(`${year}-${month}-${day}`, 'YYYY-MM-DD');
+    const duration = moment.duration(until.diff(this.now));
+    return duration.days();
 }
-
-function yearsUntil(date) {
-    const now = new Date();
-    const diff = date.getTime() - now.getTime();
-    return Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
+function hoursUntil(day, month, year) {
+    const until = moment(`${year}-${month}-${day}`, 'YYYY-MM-DD');
+    const duration = moment.duration(until.diff(this.now));
+    return duration.hours();
 }
-
-function daysUntil(date) {
-    const now = new Date();
-    const diff = date.getTime() - now.getTime();
-    return Math.floor(diff / (1000 * 60 * 60 * 24));
+function minutesUntil(day, month, year) {
+    const until = moment(`${year}-${month}-${day}`, 'YYYY-MM-DD');
+    const duration = moment.duration(until.diff(this.now));
+    return duration.minutes();
 }
-
+function secondsUntil(day, month, year) {
+    const until = moment(`${year}-${month}-${day}`, 'YYYY-MM-DD');
+    const duration = moment.duration(until.diff(this.now));
+    return duration.seconds();
+}
 
